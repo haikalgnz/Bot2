228584a91d2607704cc8f14817bf16d5b9667f4e
haikalgnz <haikalofc8@gmail.com>
2021-11-10 06:24:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ global.tum = fs.readFileSync('./src/Ah5.jpeg')
 global.tum2 = fs.readFileSync('./src/emror.jpeg')
 global.thumb = 'https://i.ibb.co/34PV5yH/By-Relldev.jpg'
 global.thumb2 = fs.readFileSync('./src/emror.jpeg')
-global.owner = ['6289681378568', '6283820073017', '6283801499848','85290882526','6285240750713','6288286421519'] // Put your number here
-global.mods = ['6285693120588'] // Want some help?
-global.prems = ['6287842147162','6285893120588','6285656615441'] // Premium user has unlimited limit
+global.owner = ['6289696454872', '6289696454872', '6289696454872','6289603042257','6289603042257','6289603042257'] // Put your number here
+global.mods = ['6289696454872'] // Want some help?
+global.prems = ['6289696454872','6289696454872','6289603042257'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
